3231931768edbb2b570e2cfa40db92fb80f460b7
racagogi <racagogi@gmail.com>
2024-08-03 17:44:39 +0900

    fix lazy.nvim clone

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,11 +1,10 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
 if not vim.uv.fs_stat(lazypath) then
-	vim.system({ "git clone",
-		"--filter=blob:none --branch=stable",
+	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable",
 		"https://github.com/folke/lazy.nvim.git",
 		lazypath })
 end
+
 
 vim.opt.rtp:prepend(lazypath)
 
